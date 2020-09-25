8193805891f15c6eede8839ca1afec96667195c4
Elias Leonhardsberger <elias.leonhardsberger@sz-ybbs.ac.at>
2020-09-25 11:52:08 +0200

    Stunde 25.9.2020

---
+++ LoggerTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LoggerTest
+{
+    public class Logger
+    {
+        private static Logger instance = null;
+
+        private Logger()
+        {
+
+        }
+
+        public void writeToConsole(String output)
+        {
+            Console.WriteLine(output);
+
+        }
+
+        public static Logger Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Logger();
+                }
+
+                return instance;
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = Logger.Instance;
+            logger.writeToConsole("Hello World");
+        }
+    }
+
+}
