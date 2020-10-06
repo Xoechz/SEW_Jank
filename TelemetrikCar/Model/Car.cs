e870423cebb57a7fdaef01683e767ff5466d1125
Elias Leonhardsberger <elias.leonhardsberger@sz-ybbs.ac.at>
2020-10-06 09:29:02 +0200

    Telemetrik begonnen, noch nichts wirkliches erreicht

---
+++ TelemetrikCar/Model/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelemetrikCar.Model
+{
+    public class Car
+    {
+        private int IdCar { get; set; }
+        private String Name { get; set; }
+        private String Typ { get; set; }
+
+    }
+}
---
+++ TelemetrikCar/Model/Telemetrik.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelemetrikCar.Model
+{
+    public class Telemetrik
+    {
+        private int IdTel { get; set; }
+        private Car Car { get; set; }
+        private double Latitude{ get; set; }
+        private double Longitude{ get; set; }
+        private double Speed{ get; set; }
+        private double Capacity{ get; set; }
+
+    }
+}
